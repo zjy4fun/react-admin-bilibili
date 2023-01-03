5b78fdb3eef91a8195b6efcb7e3643ed8b3a852a
zjy4fun <zjy4fun@gmail.com>
2023-01-03 12:05:52 +0800

    lazy loading

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,17 @@
+import React, { lazy } from 'react'
 import Home from '../views/Home'
-import About from '../views/About'
-import User from '../views/User'
-import {Navigate} from "react-router-dom" //重定向组件
+// import About from '../views/About'
+// import User from '../views/User'
+const About = lazy(() => import('../views/About'))
+const User = lazy(() => import('../views/User'))
+import { Navigate } from "react-router-dom" //重定向组件
+
+//懒加载模式需要我们添加一个 Loading 组件
 
 const routes = [
     {
         path: "/",
-        element: <Navigate to="/home"/>
+        element: <Navigate to="/home" />
     },
     {
         path: "/home",
@@ -14,11 +19,17 @@ const routes = [
     },
     {
         path: "/about",
-        element: <About />
+        element: 
+        <React.Suspense fallback={<div>Loading</div>} >
+            <About />
+        </React.Suspense>
     },
     {
         path: "/user",
-        element: <User />
+        element: 
+        <React.Suspense fallback={<div>Loading</div>} >
+            <User />
+        </React.Suspense>
     }
 ]
 
